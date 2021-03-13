b52fa38f6d7d616be433ccc0aa51f3bbfcc9c59a
ahujarahul <ahuja.rahul05@gmail.com>
2021-03-13 15:41:10 +0530

    1. added Jetpack Navigation Component

--- Recipedia/app/src/main/java/com/example/recipedia/RecipeFragment.kt
+++ Recipedia/app/src/main/java/com/example/recipedia/RecipeFragment.kt
@@ -1,13 +1,12 @@
 package com.example.recipedia
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
@@ -16,41 +15,22 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
-class RecipeListFragment : Fragment() {
+class RecipeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-          // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_recipe_list, container, false)
-
         return ComposeView(requireContext()).apply {
             setContent {
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "This is a Fragment containing list of recipes.",
+                    Text(text = "This is Recipe Fragment.",
                         fontWeight = FontWeight.Bold,
                         fontStyle = FontStyle.Italic,
                         style = TextStyle(fontSize = 24.sp))
-
-                    Spacer(modifier = Modifier.padding(8.dp))
-
-                    // navigation controller - add 'action' from nav_graph in Button click
-                    Button(onClick = { findNavController().navigate(R.id.recipeFragment) }) {
-                        Text(text = "Show Recipe")
-                    }
                 }
             }
         }
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String? = null, param2: String? = null) =
-            RecipeListFragment().apply {
-                arguments = Bundle().apply {}
-            }
     }
 }
